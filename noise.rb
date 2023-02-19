5d1bf27389593d30a077ae7149f251ab3fdb26bd
Si Doremi <sidoremi112@gmail.com>
2023-02-19 20:43:50 +0200

    released

--- noise.rb
+++ noise.rb
@@ -18,27 +18,29 @@ max_noise = 20
 default_copies = 3
 copies = ARGV[2].to_i || default_copies
 
+progressbar = ProgressBar.create(
+  title: "0/#{copies}",
+  format: '%t %a [%B] %J%% %f  ',
+  progress_mark: "=",
+  remainder_mark: " ",
+  starting_at: 0.0,
+  total: copies.to_f
+)
+
 copies.times do |i|
   video_bitrate = rand(min_bitrate...max_bitrate)
   noise = rand(min_noise...max_noise)
 
   ffmpeg_options = {
-    video_bitrate: video_bitrate,
-    custom: %W[-vf noise=alls=#{noise}:allf=t+u]
+  video_bitrate: video_bitrate,
+  custom: %W[-vf noise=alls=#{noise}:allf=t+u]
   }
 
-  progressbar = ProgressBar.create(
-    title: "#{i + 1}/#{copies}",
-    format: '%t: %f [%B] %P%%     ',
-    progress_mark: "=",
-    remainder_mark: " ",
-    starting_at: 0.0,
-    total: 1.0
-  )
-
   output_file = "#{output_prefix}#{i + 1}.mp4"
+  progressbar.title = "#{i+1}/#{copies}"
   input_file.transcode(output_file, ffmpeg_options) do |progress|
-    progressbar.progress = progress
+    progressbar.progress = progress + i
+    progressbar.progress_mark = %W(= # $ % &).sample
   end
 
   print "\r\033[K"
