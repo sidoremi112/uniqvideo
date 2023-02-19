05aa4d833d6497d8278af99f9ac1635fe0393eee
Si Doremi <sidoremi112@gmail.com>
2023-02-19 21:57:41 +0200

    updates

--- noise.rb
+++ noise.rb
@@ -1,4 +1,5 @@
 #!/usr/bin/env ruby
+
 require 'ruby-progressbar'
 require 'streamio-ffmpeg'
 
@@ -6,37 +7,33 @@ min_bitrate = 600
 max_bitrate = 1200
 min_noise = 1
 max_noise = 20
-default_copies = 3
+BEGIN {
+  default_copies = 3
+}
 copies = ARGV[2].to_i || default_copies
 black = RandomSymbolGenerator.new('\|/|')
 white = RandomSymbolGenerator.new('-')
 
 progressbar = ProgressBar.create(
-  title: "0/#{copies}",
-  format: '%t %a [%B] %J%% %f  ',
-  progress_mark: "=",
-  remainder_mark: " ",
-  starting_at: 0.0,
-  total: copies.to_f
+title: "0/#{copies}",
+format: '%t %a [%B] %J%% %f  ',
+progress_mark: "=",
+remainder_mark: " ",
+starting_at: 0.0,
+total: copies.to_f
 )
 
 input_filename = ARGV[0]
-output_prefix = ARGV[1] || File.basename(input_filename, ".*")
-
-if !File.exist?(input_filename)
-  STDERR.puts "Error: input file does not exist"
-  exit 1
-end
-
 input_file = FFMPEG::Movie.new(input_filename)
+output_prefix = ARGV[1] || File.basename(input_filename, ".*")
 
 copies.times do |i|
   video_bitrate = rand(min_bitrate...max_bitrate)
   noise = rand(min_noise...max_noise)
 
   ffmpeg_options = {
-    video_bitrate: video_bitrate,
-    custom: %W[-vf noise=alls=#{noise}:allf=t+u]
+  video_bitrate: video_bitrate,
+  custom: %W[-vf noise=alls=#{noise}:allf=t+u]
   }
 
   output_file = "#{output_prefix}#{i + 1}.mp4"
@@ -52,11 +49,25 @@ copies.times do |i|
 end
 
 BEGIN {
+  if ARGV[0].nil?
+    STDERR.puts "Usage: input_filename [output_prefix] [copies]"
+    STDERR.puts "Default [output_prefix] is input_filename without extension"
+    STDERR.puts "Default [copies] is #{default_copies}"
+    exit(1)
+  end
+
+  if !File.exist?(ARGV[0])
+    STDERR.puts "Error: input file does not exist"
+    exit 1
+  end
   class RandomSymbolGenerator
     attr_reader :symbols, :queue
 
     def initialize(data = nil)
-      @symbols = (Array(data || %w[! @ # $ % ^ & *]).join.chars.map { |x| x == '\\' ? '\\\\' : x })
+      @symbols = (Array(data || %w[! @ # $ % ^ & *]).join.chars.map{ |x| x == '\\' ? '\\\\' : x })
+      # map { |x| x == '\\' ? '\\\\' : x }
+      # this expression added as hotfix - instance of ProgressBar prints '\\' as '\'
+      # so, i just repeated it)
       @queue = symbols.dup
     end
 
